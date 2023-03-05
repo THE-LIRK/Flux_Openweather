c11293675707ff8d88d3a93633a6bd5a3f163cbc
THE-LIRK <roddyruddy11@gmail.com>
2023-03-05 17:45:50 +0100

    modification

--- k.py
+++ k.py
@@ -1,81 +1,45 @@
-# import requests
-# import pandas as pd
-# import datetime
-
-# API_KEY ="bb5f6c27cfb755afe84c05889e78ef45"
-
-# # Fonction pour récupérer les données météorologiques pour une ville donnée à une date donnée
-# def get_historical_weather(city, date):
-#     timestamp = int(datetime.datetime.strptime(date, '%Y-%m-%d').timestamp())
-#     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&dt={timestamp}&units=metric'
-#     response = requests.get(url)
-#     data = response.json()
-#     weather_data = {
-#         'city': city,
-#         'date': date,
-#         'temperature':data["main"]["temp"],
-#         'humidity': data['main']['humidity'],
-#         'wind_speed': data['wind']['speed'],
-#         "description":data["weather"][0]["description"]
-#     }
-#     return weather_data
-
-# # Liste des villes et dates pour lesquelles nous souhaitons récupérer les données météorologiques
-# cities = ['Paris', 'Lyon', 'Marseille']
-# dates = ['2022-01-01', '2022-02-17', '2022-01-03']
-
-# # Récupération des données météorologiques pour chaque ville et chaque date
-# weather_data = []
-# for city in cities:
-#     for date in dates:
-#         data = get_historical_weather(city, date)
-#         weather_data.append(data)
-
-# # Création du DataFrame correspondant aux données météorologiques historiques
-# df = pd.DataFrame.from_dict(weather_data)
-# print(df)
-
-import requests
 import pandas as pd
-from datetime import datetime, timedelta
+import time
+import requests
 
-# API URL
-url = "https://api.openweathermap.org/data/2.5/weather"
+def get_weather_data(city, api_key):
+    base_url = "https://api.openweathermap.org/data/2.5/weather"
+    query_params = {"q": city, "appid": api_key}
+    response = requests.get(base_url, params=query_params)
 
-# API Key
+    return response.json()
+
+# Remplacez "YOUR_API_KEY" par votre clé d'API OpenWeatherMap
 api_key = "bb5f6c27cfb755afe84c05889e78ef45"
 
-# List of cities to get data for
-cities = ["Paris", "Marseille", "Lyon", "Toulouse"]
+# Définissez les villes pour lesquelles vous souhaitez récupérer les données météorologiques
+cities = ["Paris", "Lyon", "Bordeaux"]
 
-# List to store the weather data for each city
+# Initialisez une liste pour stocker les données météorologiques
 weather_data = []
 
-# Dates range
-start_date = datetime.strptime("2022-01-01", "%Y-%m-%d").date()
-end_date = datetime.strptime("2023-01-01", "%Y-%m-%d").date()
-delta = timedelta(days=1)
+while True:
+    # Parcourez les villes et récupérez les données météorologiques pour chaque ville
+    for city in cities:
+        data = get_weather_data(city, api_key)
+        temp_celsius = data["main"]["temp"] - 273.15 # conversion en Celsius
+        weather_dict = {
+            "city": city,
+            "date": pd.Timestamp.now(),
+            "temp": temp_celsius,
+            "pressure": data["main"]["pressure"],
+            "humidity": data["main"]["humidity"],
+            "wind_speed": data["wind"]["speed"]
+        }
+        weather_data.append(weather_dict)
 
-# Loop through each city and get weather data for each day in the date range
-for city in cities:
-    for date in pd.date_range(start_date, end_date, freq="D"):
-        # Get the weather data for the current city and date
-        query_params = {"q": city, "appid": api_key, "units": "metric"}
-        response = requests.get(url, params=query_params)
-        data = response.json()
-        if response.status_code == 200:
-            weather_data.append({
-                "city": city,
-                "date": date,
-                "temperature": data["main"]["temp"],
-                "humidity": data["main"]["humidity"],
-                "wind_speed": data["wind"]["speed"],
-                "description": data["weather"][0]["description"]
-            })
-        else:
-            print(f"Error getting weather data for {city} on {date}: {data['message']}")
-
-# Create a pandas DataFrame from the weather data
-df = pd.DataFrame(weather_data)
-
-print(df)
+    # Créez un DataFrame à partir des données météorologiques
+    
+    df= pd.DataFrame(weather_data)
+    
+    
+    # Affichez le DataFrame en direct
+    print(df)
+    
+    # Attendez 1 min avant de récupérer de nouvelles données
+    time.sleep(60)
